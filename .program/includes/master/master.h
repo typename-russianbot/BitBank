a067f23735ca30b368a5aead3634c0998f81b53b
Matthew <axix01@yahoo.com>
2025-05-15 19:33:19 -0700

    saving... | started prototyping sha256 implementation for account passwords

---
+++ .program/includes/master/master.h
@@ -0,0 +1,20 @@
+//* @author: mr-yellow
+
+#pragma once
+
+//~ @def: Dependencies
+#include <iostream>
+#include <termios.h>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <openssl/sha.h>
+#include <filesystem>
+
+using namespace std;
+
+//~ @def: Macros
+#define _ver "BitBank v0.0.1" //& @var: _ver
+#define _clear "\033[2J"      //& @var: _clear
+
+//~ @def: Functions
