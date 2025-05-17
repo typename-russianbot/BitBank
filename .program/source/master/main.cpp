98e7898d5544f2e9a7903f3a073949e9f10d4823
Matthew <axix01@yahoo.com>
2025-05-16 19:59:07 -0700

    Created Interface for account | began implementation for account as well | saving...

--- .program/source/master/main.cpp
+++ .program/source/master/main.cpp
@@ -1,15 +1,19 @@
 #include "../../includes/account/account.h"
 #include "../../includes/data/transaction.h"
 
+//& @fn: main(void)
 int main(void)
 {
     //* @def: BitBank
     cout << "Welcome to BitBank!" << endl;
 
-    Transaction data; 
-    cout << data << endl; 
+    Transaction data;
+    cout << data << endl;
 
-    Account t("mjh2001", "passkey");
+    Account t;
+
+    if (t.search("mjh2001"))
+        cout << "search hit" << endl;
 
     cout << t << endl;
     return 0;
